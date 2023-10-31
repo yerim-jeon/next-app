e86d1452faf80764679c09d177c16ccf8a0a81f0
snaps-jerrie <jerrie@snaps.com>
2023-11-01 01:45:52 +0900

    feat: fetching user data

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,7 +1,24 @@
 import React from "react";
+interface User {
+  id: number;
+  name: string;
+}
 
-const UsersPage = () => {
-  return <div>UsersPage</div>;
+const UsersPage = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const users: User[] = await res.json();
+
+  return (
+    <>
+      <h1>Users</h1>
+      <p>{new Date().toLocaleTimeString()}</p>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </>
+  );
 };
 
 export default UsersPage;
